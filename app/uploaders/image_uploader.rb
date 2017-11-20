c2e6854b8d812f074eaf9b450de4d82617853f6c
alejandro <ale.gonzalezn@alumnos.duoc.cl>
2017-11-20 00:12:19 +0000

    img uploader achica imgs y las rota

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,21 @@
 class ImageUploader < CarrierWave::Uploader::Base
-
+  
   storage :fog
+  
+  include CarrierWave::MiniMagick
 
+  process :resize_to_fit => [400, 400]
+    
+  def extension_white_list
+    %w(jpg jpeg png gif)
+  end
+  
+  def fix_exif_rotation #this is my attempted solution
+    manipulate! do |img|
+      img.tap(&:auto_orient)
+    end
+  end
+
+  process :fix_exif_rotation
 
 end
